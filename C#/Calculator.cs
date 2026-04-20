df45f2ba61fab921be0969a3c35225555411c3f3
Adam <adam.czarycki1@gmail.com>
2026-04-20 22:11:20 +0200

    add calc
    add calc

---
+++ C#/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Jaką operację chcesz zrobić?" +
+                "\n1. Mnożenie (*)" +
+                "\n2. Dzielenie (/)" +
+                "\n3. Dodawanie (+)" +
+                "\n4. Odejmowanie (-)" +
+                "\nNapisz symbol operacji którą chcesz wykonać:");
+            char choice = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj pierwszą liczbę:");
+            double first_num = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę:");
+            double second_num = double.Parse(Console.ReadLine());
+
+            if (choice == '*')
+            {
+                Console.WriteLine($"Wynik: {first_num} * {second_num} = {first_num * second_num}");
+            }
+            else if (choice == '/')
+            {
+                Console.WriteLine($"Wynik: {first_num} / {second_num} = {first_num / second_num}");
+            }
+            else if (choice == '+')
+            {
+                Console.WriteLine($"Wynik: {first_num} + {second_num} = {first_num + second_num}");
+            }
+            else if (choice == '-')
+            {
+                Console.WriteLine($"Wynik: {first_num} - {second_num} = {first_num - second_num}");
+            }
+        }
+    }
+}
