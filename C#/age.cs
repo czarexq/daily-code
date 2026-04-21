3437081cc8e75d1f85fcb827a5a3303f328fc883
Adam <adam.czarycki1@gmail.com>
2026-04-21 20:00:50 +0200

    Create age.cs

---
+++ C#/age.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace test
+{
+    class Programs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Write your age");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hii {name}, to 100 years old left you {100 - age} years!");
+            if (age % 2 == 0)
+            {
+                Console.WriteLine("Fun fact: Your age is even numnber");
+            }
+
+        }
+
+    }
+}
