716b835cd83b07bc28f939b02aa2ce13ca458735
Adam <130793905+czarexq@users.noreply.github.com>
2026-04-21 20:10:34 +0200

    Update Calculator.cs

--- C#/Calculator.cs
+++ C#/Calculator.cs
@@ -1,38 +1,39 @@
-﻿using System;
+using System;
+
 namespace test
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Jaką operację chcesz zrobić?" +
-                "\n1. Mnożenie (*)" +
-                "\n2. Dzielenie (/)" +
-                "\n3. Dodawanie (+)" +
-                "\n4. Odejmowanie (-)" +
-                "\nNapisz symbol operacji którą chcesz wykonać:");
+            Console.WriteLine("Which operation do you want to perform?" +
+                "\n1. Multiplication (*)" +
+                "\n2. Division (/)" +
+                "\n3. Addition (+)" +
+                "\n4. Subtraction (-)" +
+                "\nEnter the symbol of the operation you want to perform:");
             char choice = char.Parse(Console.ReadLine());
 
-            Console.WriteLine("Podaj pierwszą liczbę:");
+            Console.WriteLine("Enter the first number:");
             double first_num = double.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj drugą liczbę:");
+            Console.WriteLine("Enter the second number:");
             double second_num = double.Parse(Console.ReadLine());
 
             if (choice == '*')
             {
-                Console.WriteLine($"Wynik: {first_num} * {second_num} = {first_num * second_num}");
+                Console.WriteLine($"Result: {first_num} * {second_num} = {first_num * second_num}");
             }
             else if (choice == '/')
             {
-                Console.WriteLine($"Wynik: {first_num} / {second_num} = {first_num / second_num}");
+                Console.WriteLine($"Result: {first_num} / {second_num} = {first_num / second_num}");
             }
             else if (choice == '+')
             {
-                Console.WriteLine($"Wynik: {first_num} + {second_num} = {first_num + second_num}");
+                Console.WriteLine($"Result: {first_num} + {second_num} = {first_num + second_num}");
             }
             else if (choice == '-')
             {
-                Console.WriteLine($"Wynik: {first_num} - {second_num} = {first_num - second_num}");
+                Console.WriteLine($"Result: {first_num} - {second_num} = {first_num - second_num}");
             }
         }
     }
