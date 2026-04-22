2e56c4c25ac09452473cb59cdb3d709bbbd134db
Adam <adam.czarycki1@gmail.com>
2026-04-22 17:37:06 +0200

    Create guess_num.cs

---
+++ C#/guess_num.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int computer_choise = rand.Next(1, 10);
+
+            int attempt = 1;
+
+            Console.WriteLine("Guess the number (1-10):");
+            int guess = int.Parse(Console.ReadLine());
+            Console.WriteLine(computer_choise);
+            while (guess != computer_choise)                
+
+                if (guess > computer_choise)
+                {
+                    Console.WriteLine("Incorect!!!\nAwwwh... too high, try again");
+                    guess = int.Parse(Console.ReadLine());
+                    
+                    attempt += 1;
+                }
+                else if (guess < computer_choise)
+                {
+                    Console.WriteLine("Incorect!!!\nAwwwh... too low, try again");
+                    guess = int.Parse(Console.ReadLine());
+                    attempt += 1;
+                }
+
+
+            Console.WriteLine($"Correct!! You got this at {attempt} attempt!");
+        }
+    }
+}
