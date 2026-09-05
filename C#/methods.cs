2886f9da78af106b9eaa7556825989421821f0c1
Adam Czarycki <czarexq@proton.me>
2026-09-05 12:08:28 +0200

    add arguments to methods

--- C#/methods.cs
+++ C#/methods.cs
@@ -5,18 +5,19 @@ namespace test
     {
         static void Main()
         {
+            string name = "Adam";
+            uint age = 16;
             Console.WriteLine("Hello Adam! It's your birthday!!");
-            SingHappyBirthday();
-            SingHappyBirthday();
-            SingHappyBirthday();
+            SingHappyBirthday(name, age);
+
         }
 
-        static void SingHappyBirthday()
+        static void SingHappyBirthday(string name, uint age)
         {
             Console.WriteLine("Happy Birthday to YOU!");
-            Console.WriteLine("Happy Birthday to you!");
+            Console.WriteLine("Happy Birthday to " + name + "!");
             Console.WriteLine("Happy Birthday to YOUUU!");
-            Console.WriteLine("Happy Birthday to you!");
+            Console.WriteLine("Happy Birthday to you! You are now " + age + "!");
             Console.WriteLine("");
         }
     }
