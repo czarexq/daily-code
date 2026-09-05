5a00ce13a61b1f40aee48cef6a83b8487e3d2e50
Adam Czarycki <czarexq@proton.me>
2026-09-05 11:58:41 +0200

    create methods.cs

---
+++ C#/methods.cs
@@ -0,0 +1,23 @@
+using System;
+namespace test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Adam! It's your birthday!!");
+            SingHappyBirthday();
+            SingHappyBirthday();
+            SingHappyBirthday();
+        }
+
+        static void SingHappyBirthday()
+        {
+            Console.WriteLine("Happy Birthday to YOU!");
+            Console.WriteLine("Happy Birthday to you!");
+            Console.WriteLine("Happy Birthday to YOUUU!");
+            Console.WriteLine("Happy Birthday to you!");
+            Console.WriteLine("");
+        }
+    }
+}
